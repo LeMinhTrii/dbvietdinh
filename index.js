856e3228669ebacce3f291a1691eb5e71b520c89
le minh tri <leminhtri886@gmail.com>
2023-07-06 23:40:10 +0700

    first commit

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-// Set default middlewares (logger, static, cors and no-cache)
+// Set default middlewares (logger, static, cors, and no-cache)
 server.use(middlewares);
 
 // Use default router
 server.use("/", router);
-server.listen(3000, () => {
-  console.log("JSON Server is running");
-});
+
+module.exports = server;
