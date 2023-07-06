beff5874d3f1769c8b8cd186314f819477ab6a0e
le minh tri <leminhtri886@gmail.com>
2023-07-07 00:34:30 +0700

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const fs = require("fs");
+
+// Ghi log vào tệp tin
+fs.appendFileSync("app.log", "Thông tin log của bạn");
 
 // Set default middlewares (logger, static, cors, and no-cache)
 server.use(middlewares);
